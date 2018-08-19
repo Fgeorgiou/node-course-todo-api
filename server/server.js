35a5a53a12b76d987ae2480cbcaf58e2eeb21798
Filippos Georgiou <fgeorgiou@amcstudent.edu.gr>
2018-08-19 08:38:27 +0300

    Ass POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -45,7 +45,6 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send();
     }
-
     res.send({todo});
   }, (e) => {
     res.status(400).send(e);
@@ -93,6 +92,19 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
+  });
+});
+
+app.post('/users', (req, res) =>{
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   });
 });
 
